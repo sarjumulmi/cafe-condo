abc89920f24b80b8ff1cfaa0b2f633cb492a8b8f
Sarju Mulmi <sarju.mulmi@expel.io>
2022-05-31 18:11:54 -0400

    add cache control

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,15 @@ export default Index;
 const puppeteer = require('puppeteer');
 const { login, getPaymentData } = require('../scraper');
 
-export async function getStaticProps(context) {
+// let expirestAt = Date.now();
+// let paymentData;
+
+export async function getServersideProps({ req, res }) {
+  res.setHeaders(
+    'Cache-Control',
+    'public, s-max-age=86400, stale-while-revalidate=60',
+  );
+
   const puppeteerConfig = {
     headless: true,
     args: ['--disable-setuid-sandbox'],
@@ -44,6 +52,30 @@ export async function getStaticProps(context) {
     process.env.PASSWORD,
   );
   const paymentData = await getPaymentData(page);
+
+  // todo: if caching doesn't work, make a local cache
+  // now = Date.now();
+  // if (!paymentData || expirestAt.getTime() <= now) {
+  //   const puppeteerConfig = {
+  //     headless: true,
+  //     args: ['--disable-setuid-sandbox'],
+  //     ignoreHTTPSErrors: true,
+  //   };
+
+  //   if (process.env.NODE_ENV === 'production') {
+  //     puppeteerConfig.executablePath = '/usr/bin/chromium-browser';
+  //   }
+  //   const browser = await puppeteer.launch(puppeteerConfig);
+  //   const page = await login(
+  //     browser,
+  //     process.env.BASE_URL,
+  //     process.env.EMAIL,
+  //     process.env.PASSWORD,
+  //   );
+  //   paymentData = await getPaymentData(page);
+  //   expirestAt = new Date(now + 24 * 60 * 60 * 1000);
+  // }
+
   return {
     props: { paymentData }, // will be passed to the page component as props
   };
