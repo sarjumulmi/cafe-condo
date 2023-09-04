a37daaf4d48ddda3df5895071d1725c197162eab
Sarju Mulmi <sarju.mulmi@expel.io>
2023-09-03 20:38:42 -0400

    increase timeout for load after login

--- scraper/login.js
+++ scraper/login.js
@@ -3,10 +3,12 @@ function login(browser, loginUrl, email, password) {
     try {
       const page = await browser.newPage();
       await page.goto(loginUrl);
-      await page.type('#Username', email);
-      await page.type('#Password', password);
-      await page.click('#SignIn');
-      await page.waitForSelector('#ResServicesContent ul li:first-child a');
+      await page.type("#Username", email);
+      await page.type("#Password", password);
+      await page.click("#SignIn");
+      await page.waitForSelector("#ResServicesContent ul li:first-child a", {
+        timeout: 120000,
+      });
 
       return resolve(page);
     } catch (error) {
